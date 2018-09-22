8d9461ab43fa7e3ad843eac6d7d0304d4caeeb7d
Stefan Wawrzyniak <stefanwawrzyniak@icloud.com>
2018-09-22 23:18:51 +0100

    add error handling to getHueEndpoint

--- src/hue/hue-api.js
+++ src/hue/hue-api.js
@@ -9,10 +9,23 @@ const hueBridge = axios.create({
 hueBridge.interceptors.response.use(res => {
   cacheResponse(res);
   return res
-}, err => console.log(`error at hueBridge response interception ${err}`));
+}, err => {
+  console.log(`error at hueBridge response interception ${err}`);
+  return err
+});
 
-export const getHueEndpoint = async endpoint =>
-  await getCachedResponse(endpoint) || hueBridge.get(endpoint);
+// TODO test
+export const getHueEndpoint = async endpoint => {
+  let response;
+  try {
+    response = await getCachedResponse(endpoint) || await hueBridge.get(endpoint)
+  } catch (e) {
+      console.log('caught error', e);
+      return e
+  }
+  return response
+};
+
 
 export const getLightGroups = () => getHueEndpoint('/groups');
 
