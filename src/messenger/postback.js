60f86a0393423f3329d6010f99e773854e704f92
Stefan Wawrzyniak <stefanwawrzyniak@icloud.com>
2018-11-04 18:53:39 +0000

    handle LIST_LIGHT_GROUPS postback

--- src/messenger/postback.js
+++ src/messenger/postback.js
@@ -1,11 +1,10 @@
 import hueApi from '../hue/api';
-import send from '../messenger/api';
+import send from '../messenger/send';
 
 export const handlePostback = async ({ postback, sender }) => {
-  console.log('payload', postback);
   if (postback.payload === 'LIST_LIGHT_GROUPS') {
-    const groups = await hueApi.getLightGroups();
-    console.log('groups', groups);
-    // send.textMessage(sender.id, )
+    const { data: groups } = await hueApi.getLightGroups();
+    const response = Object.keys(groups).map(key => groups[key].class)
+    send.textMessage(sender.id, response.join(', '))
   }
 };
