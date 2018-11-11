ab266873d0373a6d300039d02fb0d80ba8dcc608
Stefan Wawrzyniak <stefanwawrzyniak@icloud.com>
2018-11-11 21:33:28 +0000

    handle 404

--- src/app.js
+++ src/app.js
@@ -16,6 +16,21 @@ app.use(morgan('tiny'));
 app.disable('x-powered-by');
 
 // Routes
+
+app.use(function(req, res, next) {
+  const err = new Error('Not Found');
+  err.status = 404;
+  next(err);
+});
+
+app.use(function(err, req, res) {
+  res.locals.message = err.message;
+  res.locals.error = req.app.get('env') === 'development' ? err : {};
+
+  res.status(err.status || 500).send('Error!');
+  res.end();
+});
+
 app.use('/', index);
 app.use('/webhook', webhook);
 
